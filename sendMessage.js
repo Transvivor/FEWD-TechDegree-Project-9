f417f55e1d4f3c8be8bcaf41446a45b3ffd8a11e
Transvivor <aiden.berzins@hotmail.com>
2018-04-21 20:07:23 -0600

    Send Message with JS

--- sendMessage.js
+++ sendMessage.js
@@ -1,9 +1,14 @@
 const myForm = document.getElementById("form");
 const message = document.getElementById("submitMessage");
+const input = document.getElementById("input");
+const textarea = document.getElementById("textarea");
 
 var messageOut = function(event) {
     event.preventDefault();
-    message.innerHTML = "Message was successfully sent.";
+    if (input.value === ' ' || textarea.value === '' ){
+    message.innerHTML = "Make sure that all fields are filled"}else {
+      message.innerHTML = "Message was successfully sent";
+    }
 };
 
 myForm.addEventListener("submit", messageOut, true);
